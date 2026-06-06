758393a8e21883365a4b9b4bda1c6d64b134e287
coolkettle <SwiniawKoronie6@o2.pl>
2026-06-06 15:47:09 +0200

    add code to pass timer stopping test

--- Project/speed-split/main.gd
+++ Project/speed-split/main.gd
@@ -3,15 +3,24 @@ extends Node2D
 @onready var label: Label = %Label
 var start_timestamp: int
 var current_timestamp: int
+var stop_timestamp: int
+var stopped: bool = false
 
 func start_timer() -> void:
+	stopped = false
 	start_timestamp = Time.get_ticks_usec()
 
 func get_ticks() -> int:
-	return Time.get_ticks_usec() - start_timestamp
+	if not stopped:
+		return Time.get_ticks_usec() - start_timestamp
+	return stop_timestamp - start_timestamp
 
 func _ready() -> void:
 	start_timestamp = Time.get_ticks_usec()
+
+func stop_timer() -> void:
+	stopped = true
+	stop_timestamp = Time.get_ticks_usec()
 
 #func _process(delta: float) -> void:
 	#current_timestamp = Time.get_ticks_usec()
